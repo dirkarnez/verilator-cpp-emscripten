299eccc2cd99b5e8d3e471f6ef6d8a5e511cbebd
Dirk Arnez <smalldirkalex@gmail.com>
2025-09-25 18:34:07 +0800

    - update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-verilator --cc ./top.v && \
-emcc -lembind -o ./sim_main.js ./sim_main.cpp
+verilator --cc ./top.v || exit 1
+source /opt/emsdk/emsdk_env.sh
+emcc -O3 -std=c++14 -I/usr/local/share/verilator/include -lembind -I./ sim_main.cpp --no-entry -sERROR_ON_UNDEFINED_SYMBOLS=0 -sINITIAL_MEMORY=128kb -sALLOW_MEMORY_GROWTH -sTOTAL_STACK=64kb -o sim_main.js
